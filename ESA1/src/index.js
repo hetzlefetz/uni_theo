ec3c34a6b1155eb443cc53cce0b287a55e53c1c8
hetzlefetz <distorted.peace@gmail.com>
2021-04-26 12:34:23 +0200

    initial commit

---
+++ ESA1/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { GraphThingy } from "./graph";
+const root = document.querySelector("#asdf");
+ReactDOM.render(<GraphThingy />, root);
