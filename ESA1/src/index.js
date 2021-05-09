27db842a0a4619dfd89b7eb00b294ae45aaaa103
hetzlefetz <distorted.peace@gmail.com>
2021-05-09 20:50:42 +0200

    gh deploy

--- ESA1/src/index.js
+++ ESA1/src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Frame } from "./frame";
-const root = document.querySelector("#asdf");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Frame } from './frame';
+const root = document.querySelector('#app');
 ReactDOM.render(<Frame />, root);
--- ESA1/webpack.config.js
+++ ESA1/webpack.config.js
@@ -1,3 +1,5 @@
+const CreateFileWebpack = require('create-file-webpack');
+
 module.exports = {
   devtool: 'source-map',
   module: {
@@ -13,4 +15,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CreateFileWebpack({
+      path: './dist',
+      fileName: 'index.html',
+      content:
+        '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>ESA 1</title></head><body><div id="app"></div><script src="./main.js"></script></body></html>',
+    }),
+  ],
 };
