819b09e8b9af7a3a4d2ba78a1de466056f22357a
hetzlefetz <distorted.peace@gmail.com>
2021-06-03 22:31:33 +0200

    ESA3 Initial commit
    - working automaton.
    todo styling

---
+++ ESA3/automaton.js
@@ -0,0 +1,165 @@
+const q1 = {
+  name: 'q1',
+  follower: ['q2', 'q3'],
+  isEnd: false,
+};
+const q2 = {
+  name: 'q2',
+  follower: ['q3', 'q4'],
+  isEnd: true,
+};
+const q3 = {
+  name: 'q3',
+  follower: ['q2', 'q3', 'q4'],
+  isEnd: true,
+};
+const q4 = {
+  name: 'q4',
+  follower: ['q2', 'q3'],
+  isEnd: false,
+};
+const rules = {
+  'q1,q2': {
+    read: 'Z',
+    readCellar: null,
+    writeCellar: null,
+    next: q2,
+  },
+  'q1,q3': {
+    read: '(',
+    readCellar: null,
+    writeCellar: 'X',
+    next: q3,
+  },
+  'q2,q4': {
+    read: 'O',
+    readCellar: null,
+    writeCellar: null,
+    next: q4,
+  },
+  'q2,q3': {
+    read: ')',
+    readCellar: 'X',
+    writeCellar: null,
+    next: q3,
+  },
+  'q3,q2': {
+    read: 'Z',
+    readCellar: null,
+    writeCellar: null,
+    next: q2,
+  },
+  'q3,q3': [
+    {
+      read: '(',
+      readCellar: null,
+      writeCellar: 'X',
+      next: q3,
+    },
+    {
+      read: ')',
+      readCellar: 'X',
+      writeCellar: null,
+      next: q3,
+    },
+  ],
+  'q3,q4': {
+    read: 'O',
+    readCellar: null,
+    writeCellar: null,
+    next: q4,
+  },
+  'q4,q2': {
+    read: 'Z',
+    readCellar: null,
+    writeCellar: null,
+    next: q2,
+  },
+
+  'q4,q3': {
+    read: '(',
+    readCellar: null,
+    writeCellar: 'X',
+    next: q3,
+  },
+};
+
+class Automaton {
+  cellar = [];
+  currentState = null;
+  word = '';
+  constructor(word) {
+    this.currentState = q1;
+    this.word = word;
+  }
+
+  finished() {
+    return this.word.length == 0 && this.currentState.isEnd;
+  }
+
+  read(input, readType) {
+    switch (readType) {
+      case 'Z':
+        return (
+          input == '0' ||
+          input == '1' ||
+          input == '2' ||
+          input == '3' ||
+          input == '4' ||
+          input == '5' ||
+          input == '6' ||
+          input == '7' ||
+          input == '8' ||
+          input == '9'
+        );
+      case 'O':
+        return input == '+' || input == '-' || input == '*' || input == '/';
+      case '(':
+      case ')':
+        return input == readType;
+      default:
+        return false;
+    }
+  }
+
+  nextState() {
+    if (this.word.length == 0) return false;
+    var input = this.word.charAt(0);
+
+    for (var i = 0; i < this.currentState.follower.length; i++) {
+      var rulesByName =
+        rules[`${this.currentState.name},${this.currentState.follower[i]}`];
+
+      if (!rulesByName) {
+        return false;
+      }
+      if (!Array.isArray(rulesByName)) {
+        rulesByName = [rulesByName];
+      }
+      for (var j = 0; j < rulesByName.length; j++) {
+        var rule = rulesByName[j];
+        if (this.read(input, rule.read) && !rule.readCellar) {
+          this.word = this.word.substring(1);
+          this.currentState = rule.next;
+          if (rule.writeCellar) {
+            this.cellar.push(rule.writeCellar);
+          }
+          return true;
+        }
+        if (this.read(input, rule.read) && rule.readCellar) {
+          var cellarValue = this.cellar.pop();
+          if (!cellarValue) {
+            return false;
+          }
+          this.word = this.word.substring(1);
+          this.currentState = rule.next;
+          if (rule.writeCellar) {
+            this.cellar.push(rule.writeCellar);
+          }
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
---
+++ ESA3/main.js
@@ -0,0 +1,400 @@
+var appStates = {
+  PAUSED: 'PAUSED',
+  RUNNING: 'RUNNING',
+  STEP: 'STEP',
+  STOPPED: 'STOPPED',
+};
+var speed = {
+  Langsam: '5000',
+  Normal: '1000',
+  Schnell: '500',
+};
+var appState = appStates.STOPPED;
+var currentSpeed = speed.Normal;
+
+const initialState = Object.freeze('F');
+var currentState = initialState;
+var pauseRequested = false;
+var currentStep = 0;
+const states = Object.freeze({
+  S: {
+    followers: ['FOF', 'A', 'Z'],
+  },
+  Z: {
+    followers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  },
+  O: {
+    followers: ['+', '-', '*', '/'],
+  },
+  A: {
+    followers: ['(F)'],
+  },
+});
+/**
+ * Sets up the buttons according tio a passed state
+ * @param {string} state
+ */
+const setupButtonStates = () => {
+  switch (appState) {
+    case appStates.PAUSED:
+      enableButton('btn-play', 'btn-cancel');
+      disableButton(
+        'btn-pause',
+        'btn-step',
+        'drp-speed',
+        'btn-genInvalid',
+        'btn-tour',
+        'btn-doc',
+      );
+      break;
+    case appStates.RUNNING:
+      enableButton('btn-pause', 'btn-cancel');
+      disableButton(
+        'btn-play',
+        'btn-step',
+        'drp-speed',
+        'btn-genInvalid',
+        'btn-tour',
+        'btn-doc',
+      );
+      break;
+    case appStates.STEP:
+      enableButton('btn-cancel', 'btn-step');
+      disableButton(
+        'btn-play',
+        'btn-pause',
+        'drp-speed',
+        'btn-genInvalid',
+        'btn-tour',
+        'btn-doc',
+      );
+      break;
+    case appStates.STOPPED:
+      enableButton(
+        'btn-play',
+        'btn-step',
+        'drp-speed',
+        'btn-genInvalid',
+        'btn-tour',
+        'btn-doc',
+      );
+      disableButton('btn-pause', 'btn-cancel');
+      break;
+  }
+};
+
+async function dostuff() {
+  var aut = new Automaton('(1+1)+(1/(1*1))');
+  var fin = false;
+  while (aut.nextState()) {
+    if (aut.finished()) fin = true;
+  }
+  if (fin) {
+    console.log(aut);
+    console.log('Yay!');
+  } else {
+    console.log('Oh noes ... ');
+  }
+}
+async function tourClick() {
+  introJs()
+    .setOptions({
+      steps: [
+        {
+          intro: 'Intro für den Arithmetik Generator',
+        },
+        {
+          element: document.querySelector('#btn-play'),
+          intro: 'Startet das automatische generieren der Arithmetik',
+        },
+        {
+          element: document.querySelector('#btn-pause'),
+          intro:
+            'Pausiert die generierung. Zum erneuten starten einfach Play drücken',
+        },
+        {
+          element: document.querySelector('#btn-step'),
+          intro: 'Startet das schrittweise generieren der Arithmetik',
+        },
+        {
+          element: document.querySelector('#btn-cancel'),
+          intro:
+            'Bricht das generieren ab und versetzt die Applikation in den Initialzustand',
+        },
+        {
+          element: document.querySelector('#drp-speed'),
+          intro: 'Hier kann die Animationsgeschwindigkeit gewähltwerden',
+        },
+        {
+          element: document.querySelector('#btn-genInvalid'),
+          intro:
+            'Generiert einen ungüligen ausdruck indem ein gültiger ausdruck generiert wird und ein zeichen gelöscht wird',
+        },
+        {
+          element: document.querySelector('#btn-tour'),
+          intro: 'Startet dieses Tutorial',
+        },
+        {
+          element: document.querySelector('#btn-doc'),
+          intro: 'Weiterführende informationen zu dieser Anwendung',
+        },
+        {
+          element: document.querySelector('#output'),
+          intro:
+            'Hier erscheint der generierte ausdruck / werden status informationen angezeigt',
+        },
+        {
+          element: document.querySelector('#output-table'),
+          intro: 'Hier kann die generierung schrittweise nachvollzogen werden',
+        },
+        {
+          element: document.querySelector('#grammar'),
+          intro: 'Die Grammatik die dieser anwendung zu Grunde liegt',
+        },
+      ],
+    })
+    .start();
+}
+async function pauseClick() {
+  document.getElementById('output').innerHTML =
+    'Generierter Ausdruck : \n Pausiert';
+  pauseRequested = true;
+}
+async function stepClick() {
+  appState = appStates.STEP;
+  setupButtonStates();
+  document.getElementById('output').innerHTML =
+    'Generierter Ausdruck : \n Auf > clicken um den nächsten schritt durchzuführen';
+  currentState = nextState(currentState);
+  if (isTerminal(currentState)) {
+    markLastRow();
+    disableButton('btn-step');
+    alert('Ausdruck erfolgreich generiert');
+    document.getElementById('output').innerHTML =
+      'Generierter Ausdruck : \n' + currentState;
+  }
+}
+async function playClick() {
+  pauseRequested = false;
+  if (appState != appStates.PAUSED) {
+    clearTable(); //Only clear table if we start a new sim
+  }
+  appState = appStates.RUNNING;
+  setupButtonStates();
+  document.getElementById('output').innerHTML =
+    'Generierter Ausdruck : \n Generiere ...';
+  var output = await genValid();
+
+  document.getElementById('output').innerHTML =
+    'Generierter Ausdruck : \n' + output.currentState;
+  appState = appStates.STOPPED;
+  setupButtonStates();
+}
+
+async function genValid() {
+  while (!isTerminal(currentState)) {
+    currentState = nextState(currentState);
+    if (currentState.length > 10000) {
+      alert('Maximale länge überschritten');
+      break;
+    }
+    if (pauseRequested) {
+      break;
+    }
+    await new Promise((r) =>
+      setTimeout(r, document.getElementById('drp-speed').value),
+    );
+  }
+
+  var output = {
+    currentState,
+    numberOpen: (currentState.match(/\(/g) || []).length,
+    numberClosed: (currentState.match(/\)/g) || []).length,
+    isValid: isTerminal(currentState) && eval(currentState),
+  };
+  if (!pauseRequested) {
+    markLastRow();
+  }
+  return output;
+}
+function markLastRow() {
+  var table = document.getElementById('output-table');
+  var rows = table.getElementsByTagName('tr');
+
+  rows[rows.length - 1].className = 'success-row';
+}
+
+function prependOps(op, operations) {
+  for (var i = 0; i < operations.length; i++) {
+    operations[i] = op + '->' + operations[i];
+  }
+}
+
+function isTerminal(value) {
+  if (value.includes('A')) return false;
+  if (value.includes('F')) return false;
+  if (value.includes('Z')) return false;
+
+  return true;
+}
+
+function nextState(state, addToTable = true) {
+  var oldState = state;
+  var operations = [];
+  state = state.replaceAll('F', () => {
+    followers = shuffle(states.S.followers);
+    operations.push(followers[0]);
+    return followers[0];
+  });
+  if (addToTable && operations.length > 0) {
+    prependOps('F', operations);
+    insertRow(++currentStep, operations.join(','), oldState, state);
+  }
+  oldState = state;
+  operations = [];
+  state = state.replaceAll('O', () => {
+    followers = shuffle(states.O.followers);
+    operations.push(followers[0]);
+    return followers[0];
+  });
+  if (addToTable && operations.length > 0) {
+    prependOps('O', operations);
+    insertRow(++currentStep, operations.join(','), oldState, state);
+  }
+  oldState = state;
+  operations = [];
+
+  state = state.replaceAll('A', () => {
+    followers = shuffle(states.A.followers);
+    operations.push(followers[0]);
+    return followers[0];
+  });
+  if (addToTable && operations.length > 0) {
+    prependOps('A', operations);
+    insertRow(++currentStep, operations.join(','), oldState, state);
+  }
+  oldState = state;
+  operations = [];
+
+  state = state.replaceAll('Z', () => {
+    var val = getRandomIntInclusive(0, 9);
+    operations.push(val);
+    return val;
+  });
+  if (addToTable && operations.length > 0) {
+    prependOps('Z', operations);
+    insertRow(++currentStep, operations.join(','), oldState, state);
+  }
+  return state;
+}
+
+function genInvalid() {
+  pauseRequested = false;
+  if (appState != appStates.PAUSED) {
+    clearTable(); //Only clear table if we start a new sim
+  }
+  appState = appStates.RUNNING;
+  setupButtonStates();
+
+  var state = currentState;
+  while (!isTerminal(state)) {
+    state = nextState(state, false);
+    if (state.length > 10000) {
+      alert('Maximale länge überschritten');
+      break;
+    }
+    if (pauseRequested) {
+      break;
+    }
+  }
+  if (state.length == 1) {
+    state = '-';
+  } else {
+    state = removeByIndex(getRandomIntInclusive(0, state.length - 1));
+  }
+  document.getElementById('output').innerHTML =
+    'Generierter ungültiger Ausdruck : \n' + state;
+  appState = appStates.STOPPED;
+  setupButtonStates();
+}
+function removeByIndex(str, index) {
+  return str.slice(0, index) + str.slice(index + 1);
+}
+/**
+ *
+ * Helper functions
+ *
+ */
+/**
+ * Performs a fisher yates shuffle of a given array
+ * @param {any[]} array
+ * @returns the shuffled array
+ */
+function shuffle(array) {
+  var m = array.length,
+    t,
+    i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+/**
+ * Gets a random number in the range of R=[min,max]
+ * @param {number} min
+ * @param {number} max
+ * @returns a random number
+ */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+/**
+ * Adds the disabled class to elements identified by id
+ * @param  {...string} ids
+ */
+function disableButton(...ids) {
+  ids.forEach((id) => document.getElementById(id).classList.add('disabled'));
+}
+
+/**
+ * Remove the disabled class to elements identified by id
+ * @param  {...string} ids
+ */
+function enableButton(...ids) {
+  ids.forEach((id) => document.getElementById(id).classList.remove('disabled'));
+}
+function insertRow(step, rule, oldstate, newstate) {
+  var table = document.getElementById('output-table');
+  var row = table.insertRow();
+  var cell0 = row.insertCell(0);
+  cell0.innerHTML = step;
+  var cell1 = row.insertCell(1);
+  cell1.innerHTML = oldstate;
+  var cell2 = row.insertCell(2);
+  cell2.innerHTML = newstate;
+  var cell3 = row.insertCell(3);
+  cell3.innerHTML = rule;
+}
+
+function clearTable() {
+  var node = document.getElementById('output-table-body');
+  while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
+}
+// window.onload = (event) => {
+//   setupButtonStates();
+// };
